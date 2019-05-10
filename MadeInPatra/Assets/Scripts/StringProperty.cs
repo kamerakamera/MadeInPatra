7a23befd727481435ec975fb28090ef5f8ed3ae0
kamera <asahi20000124@icloud.com>
2019-05-10 23:28:28 +0900

    StringPropertyを追加

---
+++ MadeInPatra/Assets/Scripts/StringProperty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class StringProperty
+{
+    public static string[] expressionName = new string[]{
+        "normal"/*通常*/,"smile"/*笑顔*/,"closeEyes"/*目をつむる*/,"sumg"/*どや顔*/,"impatience"/*焦り*/,"surprise"/*驚き*/,"troubled"/*困り顔*/,"sadness"/*悲しみ*/,"anger"/*怒り*/,"doubt"/*疑問*/,"beforeButtle"/*戦闘前*/,"damned"/*呆れ顔*/,"grin"/*にやけ顔*/,"ashamed"/*恥じらい*/
+    };
+    public static string[] stillNames = new string[]{//Stillの名前を事前に格納
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    };
+}
