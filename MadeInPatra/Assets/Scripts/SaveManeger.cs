a46fde6d42941f0b461198dbc95dde2a40b12065
kamera <asahi20000124@icloud.com>
2019-05-10 23:22:38 +0900

    SaveDataClassを削除、SaveにはPlayerPrefを使用するように仕様変更

--- MadeInPatra/Assets/Scripts/SaveManeger.cs
+++ MadeInPatra/Assets/Scripts/SaveManeger.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveManeger : MonoBehaviour
 {
+    [SerializeField]
+    private Image[] stills;
+    [SerializeField]
+    private Button[] loadButton;//戦闘終了後にPlayerPrefにKey追加
+    private void Awake()
+    {
+        for (int i = 0; i < StringProperty.stillNames.Length; i++)
+        {
+            if (PlayerPrefs.HasKey(StringProperty.stillNames[i]))
+            {
+                //Still表示
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
