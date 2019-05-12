a69bf1c7cd2beb7cb5f057e7bf10cfee35955769
kamera <asahi20000124@icloud.com>
2019-05-13 04:35:36 +0900

    StringPropertyにloadSceneNameを追加、Sceneの変更を適用

--- MadeInPatra/Assets/Scripts/StringProperty.cs
+++ MadeInPatra/Assets/Scripts/StringProperty.cs
@@ -4,17 +4,57 @@ using System.Collections.Generic;
 public class StringProperty
 {
     public static string[] expressionName = new string[]{
-        "normal"/*通常*/,"smile"/*笑顔*/,"closeEyes"/*目をつむる*/,"sumg"/*どや顔*/,"impatience"/*焦り*/,"surprise"/*驚き*/,"troubled"/*困り顔*/,"sadness"/*悲しみ*/,"anger"/*怒り*/,"doubt"/*疑問*/,"beforeButtle"/*戦闘前*/,"damned"/*呆れ顔*/,"grin"/*にやけ顔*/,"ashamed"/*恥じらい*/
+        "normal"/*通常*/,
+        "smile"/*笑顔*/,
+        "closeEyes"/*目をつむる*/,
+        "sumg"/*どや顔*/,
+        "impatience"/*焦り*/,
+        "surprise"/*驚き*/,
+        "troubled"/*困り顔*/,
+        "sadness"/*悲しみ*/,
+        "anger"/*怒り*/,
+        "doubt"/*疑問*/,
+        "beforeBattle"/*戦闘前*/,
+        "damned"/*呆れ顔*/,
+        "grin"/*にやけ顔*/,
+        "ashamed"/*恥じらい*/
     };
     public static string[] stillNames = new string[]{//Stillの名前を事前に格納
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11"
+    };
+
+    public static string[] loadSceneName = new string[]{
+        "1-1",
+        "1-2",
+        "Tutorial-before",
+        "Tutorial-battle",
+        "Tutorial-after",
+        "Eli-before",
+        "Eli-battle",
+        "Charlotte-after",
+        "Charlotte-before",
+        "Charlotte-battle",
+        "Mico-after",
+        "Mico-before",
+        "Mico-battle",
+        "Mary-after",
+        "Mary-before",
+        "Mary-battle",
+        "Nanashi-after",
+        "Nanashi-before",
+        "Nanashi-battle",
+        "Final",
+        "Ending",
     };
 }
