f4953c3beef6f6ed9bb148991d0228a2d855d153
kamera <asahi20000124@icloud.com>
2019-05-17 01:41:52 +0900

    BackImageManegerを追加、それに伴った背景画像の変更と背景画像のFade表示機能を追加

---
+++ MadeInPatra/Assets/Scripts/Novel/BackImageManeger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackImageManeger : MonoBehaviour
+{
+    [SerializeField]
+    private Image backGround, whiteImage;
+    [SerializeField]
+    private Sprite[] backGroundImageList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteImage.color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetBackGroundImage(int num)
+    {
+        backGround.sprite = backGroundImageList[num];
+    }
+
+    public void SetFadeColor(int num)
+    {
+        if (num == 0) { whiteImage.color = new Color(0, 0, 0, 0); }
+        else { whiteImage.color = new Color(1, 1, 1, 0); }
+    }
+
+    public void FadeView(int time)
+    {
+        StartCoroutine(FadeViewCor(time));
+    }
+
+    private IEnumerator FadeViewCor(int time)
+    {
+        float count = 0;
+        while (count <= time)
+        {
+            count += Time.deltaTime;
+            whiteImage.color = new Color(whiteImage.color.r, whiteImage.color.g, whiteImage.color.b, 1 - count / time);
+            yield return null;
+        }
+        yield break;
+    }
+
+    public void FadeOut(int time)
+    {
+        StartCoroutine(FadeOutCor(time));
+    }
+    private IEnumerator FadeOutCor(int time)
+    {
+        float count = 0;
+        while (count <= time)
+        {
+            Debug.Log(count);
+            count += Time.deltaTime;
+            whiteImage.color = new Color(whiteImage.color.r, whiteImage.color.g, whiteImage.color.b, count / time);
+            yield return null;
+        }
+        yield break;
+    }
+}
