5fe5d545877e24cabe1f291fa1f125423d965dca
kamera <asahi20000124@icloud.com>
2019-06-22 00:49:07 +0900

    LoadLetters、LetterSceneManegerを追加

---
+++ MadeInPatra/Assets/Scripts/LetterSceneManeger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LetterSceneManeger : MonoBehaviour
+{
+    [SerializeField]
+    private Animation fadeScreen;
+    [SerializeField]
+    private AnimationClip[] fadeAnimation;
+    List<Sprite> letters = new List<Sprite>();
+    [SerializeField]
+    private GameObject letterPanel, selectPanel;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        fadeScreen.gameObject.SetActive(true);
+        fadeScreen.clip = fadeAnimation[0];
+        fadeScreen.Play();
+        SwitchFadeScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SwitchFadeScreen()
+    {
+        fadeScreen.gameObject.SetActive(!fadeScreen.gameObject.activeSelf);
+    }
+
+    public void OnMemberClick(string memberName)
+    {
+        int letterCount = LoadLetters.Instance.LetterForHoneyStrap[memberName].Length;
+        for (int i = 0; i < letterCount; i++)
+        {
+            letters.Add(LoadLetters.Instance.LetterForHoneyStrap[memberName][i]);
+        }
+    }
+
+    private void Switch(){
+        
+    }
+
+    public void OnReturnTitleClick(string sceneName)
+    {
+        StartCoroutine(OnReturnTitleClickCor(sceneName));
+    }
+
+    private IEnumerator OnReturnTitleClickCor(string sceneName)
+    {
+        SwitchFadeScreen();
+        fadeScreen.clip = fadeAnimation[1];
+        fadeScreen.Play("FadeView");
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ MadeInPatra/Assets/Scripts/LoadLetters.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLetters : Singleton<LoadLetters>
+{
+    public Dictionary<string, Sprite[]> LetterForHoneyStrap { get; private set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        LetterForHoneyStrap = new Dictionary<string, Sprite[]>()
+        {
+            {"patra",Resources.LoadAll<Sprite>("Letters/patra")},
+            {"eli",Resources.LoadAll<Sprite>("Letters/eli")},
+            {"mico",Resources.LoadAll<Sprite>("Letters/mico")},
+            {"charlotte",Resources.LoadAll<Sprite>("Letters/charlotte")},
+            {"mary",Resources.LoadAll<Sprite>("Letters/mary")}
+        };
+        foreach (var item in LetterForHoneyStrap)
+        {
+            if (item.Value.Length <= 0)
+            {
+                Debug.Log("LetterLoadErrer " + item.Key);
+                continue;
+            }
+            Resources.UnloadUnusedAssets();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
