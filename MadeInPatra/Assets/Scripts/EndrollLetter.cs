d89efca9d9206df22d48c2a03d4d36c75538640e
kamera <asahi20000124@icloud.com>
2019-07-22 01:54:11 +0900

    EndrollLetter.csを追加

---
+++ MadeInPatra/Assets/Scripts/EndrollLetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndrollLetter : MonoBehaviour
+{
+    [SerializeField]
+    private Image image;
+    private Sprite imageList;
+    private int letterCount, viewLetterNum = 8;//手紙の枚数
+
+    private void Start()
+    {
+
+    }
+
+    private void SetLetter()
+    {
+        var letter = Resources.Load<Sprite>("Letter/patra/" + "letter" + Random.Range(0, letterCount));//手紙の名前を"letter" + "0~手紙の枚数文"に名前指定
+        if (letter == null)
+        {
+            Debug.Log("nothing letter");
+            return;
+        }
+        image.sprite = letter;
+        Resources.UnloadAsset(letter);
+    }
+
+}
