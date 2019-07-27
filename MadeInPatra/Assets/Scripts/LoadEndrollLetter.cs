0efa0a1b3ff99c4194e1504b9b4b9c125662ff9a
kamera <asahi20000124@icloud.com>
2019-07-28 00:55:48 +0900

    StringPropertyに設定したランダム配列からランダムに選択して手紙の画像をLoadするScriptsを追加

---
+++ MadeInPatra/Assets/Scripts/LoadEndrollLetter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadEndrollLetter : Singleton<LoadEndrollLetter>
+{
+    public Sprite[] LoadSprite { get; private set; } = new Sprite[8];
+
+    private void Awake()
+    {
+        int randNum = Random.Range(0, StringProperty.letterList.GetLength(0));
+        int loadCount = 0;
+        for (int i = 0; i < StringProperty.letterList[randNum].Length; i++)
+        {
+            for (int j = 1; j < StringProperty.letterCount[StringProperty.letterList[randNum][i]] + 1; j++)
+            {
+                var letter = Resources.Load<Sprite>("Letters/patra/letter" + StringProperty.letterList[randNum][i] + "-" + j);
+                if (letter == null)
+                {
+                    Debug.Log(StringProperty.letterList[randNum][i] + "-" + j + "loadError");
+                    break;
+                }
+                Debug.Log(letter.name + "loaded!");
+                LoadSprite[loadCount] = letter;
+                Resources.UnloadAsset(letter);
+                loadCount++;
+            }
+        }
+    }
+}
